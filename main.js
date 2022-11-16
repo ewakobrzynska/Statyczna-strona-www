f4ec6af138a6861fcbbdabe8e45560c2b75b187a
ewakobrzynska <ewakobrzynska1709@gmail.com>
2022-11-17 00:30:09 +0100

    v6

--- main.js
+++ main.js
@@ -6,3 +6,21 @@ switcher.addEventListener('click', function() {
     document.body.classList.toggle('default-theme');
     document.body.classList.toggle('dark-theme');
 });
+
+
+/*function toggleMenu(){
+
+    console.log("toggleMenu called!");
+
+    let menu = document.getElementById("nav_menu");
+
+    // console.log(menu.className)
+
+    if (menu.className === "nav_menu"){
+        menu.className += " responsive";
+    }else{
+        menu.className = "nav_menu";
+    }
+
+
+}*/
